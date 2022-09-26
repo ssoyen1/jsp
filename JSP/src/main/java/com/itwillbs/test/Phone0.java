92068701bd60ae6b4e4e68e18805c8ceec701d78
lee soyeon <ssoyen1@naver.com>
2022-09-26 23:27:41 +0900

    서블릿 객체 저장클래스

---
+++ JSP/src/main/java/com/itwillbs/test/Phone0.java
@@ -0,0 +1,22 @@
+package com.itwillbs.test;
+
+public class Phone0 {
+		// 모델명, 전화번호
+		private String model;
+		private String tel;
+		
+		
+		// alt shift s +r / getter setter 만들어주기
+		public String getModel() {
+			return model;
+		}
+		public void setModel(String model) {
+			this.model = model;
+		}
+		public String getTel() {
+			return tel;
+		}
+		public void setTel(String tel) {
+			this.tel = tel;
+		}
+}
