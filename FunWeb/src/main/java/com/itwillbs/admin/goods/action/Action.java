ae29c6d1cc9f47317cb10c6561f9f9c17445d0a3
lee soyeon <ssoyen1@naver.com>
2022-10-21 19:34:36 +0900

    FunWeb / MVC 적용연습 / 상품판매 : Action, ActionForward

---
+++ FunWeb/src/main/java/com/itwillbs/admin/goods/action/Action.java
@@ -0,0 +1,12 @@
+package com.itwillbs.admin.goods.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Action {
+		// 상수, 추상메서드만 선언가능
+		
+	public ActionForward execute(HttpServletRequest request,
+								 HttpServletResponse response) throws Exception; // 실행할때 에러발생할 수 있으므로 에러처리
+	
+}
